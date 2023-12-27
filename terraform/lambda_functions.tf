ddfa1716f483df856c562b8fc6af5408ceb6d72f
Artin-Mirzayans <50967799+Artin-Mirzayans@users.noreply.github.com>
2023-12-27 11:32:36 -0800

    /terraform path module

--- terraform/lambda_functions.tf
+++ terraform/lambda_functions.tf
@@ -177,11 +177,12 @@ resource "aws_iam_role" "lambda_delete_role" {
 }
 
 resource "aws_lambda_function" "weather_router" {
-  function_name = "WeatherRouter"
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  timeout       = 6
-  filename      = "lambda_functions/WeatherRouter/function.zip"
+  function_name    = "WeatherRouter"
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  timeout          = 6
+  filename         = "${path.module}/terraform/lambda_functions/WeatherRouter.zip"
+  source_code_hash = base64sha256(file("${path.module}/terraform/lambda_functions/WeatherRouter.zip"))
 
   role = aws_iam_role.lambda_weather_router_role.arn
 }
@@ -190,8 +191,8 @@ resource "aws_lambda_function" "fetch_weather_data" {
   function_name    = "FetchWeatherData"
   handler          = "index.handler"
   runtime          = "nodejs18.x"
-  filename         = "${path.module}/lambda_functions/FetchWeatherData.zip"
-  source_code_hash = base64sha256(file("${path.module}/lambda_functions/FetchWeatherData.zip"))
+  filename         = "${path.module}/terraform/lambda_functions/FetchWeatherData.zip"
+  source_code_hash = base64sha256(file("${path.module}/terraform/lambda_functions/FetchWeatherData.zip"))
 
   environment {
     variables = {
@@ -206,8 +207,8 @@ resource "aws_lambda_function" "get_weather_data" {
   function_name    = "GetWeatherData"
   handler          = "index.handler"
   runtime          = "nodejs18.x"
-  filename         = "${path.module}/lambda_functions/GetWeatherData.zip"
-  source_code_hash = base64sha256(file("${path.module}/lambda_functions/GetWeatherData.zip"))
+  filename         = "${path.module}/terraform/lambda_functions/GetWeatherData.zip"
+  source_code_hash = base64sha256(file("${path.module}/terraform/lambda_functions/GetWeatherData.zip"))
 
   role = aws_iam_role.lambda_get_role.arn
 }
@@ -216,8 +217,8 @@ resource "aws_lambda_function" "insert_weather_data" {
   function_name    = "InsertWeatherData"
   handler          = "index.handler"
   runtime          = "nodejs18.x"
-  filename         = "${path.module}/lambda_functions/InsertWeatherData.zip"
-  source_code_hash = base64sha256(file("${path.module}/lambda_functions/InsertWeatherData.zip"))
+  filename         = "${path.module}/terraform/lambda_functions/InsertWeatherData.zip"
+  source_code_hash = base64sha256(file("${path.module}/terraform/lambda_functions/InsertWeatherData.zip"))
 
   role = aws_iam_role.lambda_insert_role.arn
 }
@@ -226,8 +227,8 @@ resource "aws_lambda_function" "update_weather_data" {
   function_name    = "UpdateWeatherData"
   handler          = "index.handler"
   runtime          = "nodejs18.x"
-  filename         = "${path.module}/lambda_functions/UpdateWeatherData.zip"
-  source_code_hash = base64sha256(file("${path.module}/lambda_functions/UpdateWeatherData.zip"))
+  filename         = "${path.module}/terraform/lambda_functions/UpdateWeatherData.zip"
+  source_code_hash = base64sha256(file("${path.module}/terraform/lambda_functions/UpdateWeatherData.zip"))
 
   role = aws_iam_role.lambda_insert_role.arn
 }
@@ -236,8 +237,8 @@ resource "aws_lambda_function" "get_all_weather_data" {
   function_name    = "GetAllWeatherData"
   handler          = "index.handler"
   runtime          = "nodejs18.x"
-  filename         = "${path.module}/lambda_functions/GetAllWeatherData.zip"
-  source_code_hash = base64sha256(file("${path.module}/lambda_functions/GetAllWeatherData.zip"))
+  filename         = "${path.module}/terraform/lambda_functions/GetAllWeatherData.zip"
+  source_code_hash = base64sha256(file("${path.module}/terraform/lambda_functions/GetAllWeatherData.zip"))
 
   role = aws_iam_role.lambda_query_role.arn
 }
@@ -246,8 +247,8 @@ resource "aws_lambda_function" "delete_weather_data" {
   function_name    = "DeleteWeatherData"
   handler          = "index.handler"
   runtime          = "nodejs18.x"
-  filename         = "${path.module}/lambda_functions/DeleteWeatherData.zip"
-  source_code_hash = base64sha256(file("${path.module}/lambda_functions/DeleteWeatherData.zip"))
+  filename         = "${path.module}/terraform/lambda_functions/DeleteWeatherData.zip"
+  source_code_hash = base64sha256(file("${path.module}/terraform/lambda_functions/DeleteWeatherData.zip"))
 
   role = aws_iam_role.lambda_delete_role.arn
 }
